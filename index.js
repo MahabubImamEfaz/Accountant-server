e266273ea1987fe74e09d9facc3a695f76ac281e
MahabubImamEfaz <imtiyaz.efaz@gmail.com>
2022-11-10 22:30:40 +0600

    delete review

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ async function run() {
       const reviews = await cursor.toArray();
       res.send(reviews);
     });
-
+    //delte review
     app.delete("/reviews/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
